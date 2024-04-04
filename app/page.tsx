2878bd91fe5a9fbc6af10e5cde06518f7dedfd30
sunny <sunnya2023@gmail.com>
2024-04-05 00:26:23 +0900

    1st form modify

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 export default function Home() {
   return (
     <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100  h-screen flex items-center justify-center p-5 ">
-      <div className="bg-white p-5 shadow-lg rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2">
+      <div
+        className="bg-white p-5 shadow-lg rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2 *:outline-none
+        ring ring-transparent transition-shadow
+        has-[:invalid]:bg-red-100 
+      has-[:invalid]:ring
+      "
+      >
         <input
-          className="w-full rounded-full h-12 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer"
-          type="email"
+          className="w-full rounded-full h-12 bg-gray-200 pl-5  ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer"
+          type="text"
           required
           placeholder="Email address"
         />
@@ -13,9 +19,8 @@ export default function Home() {
         </span>
         <button
           className="bg-black text-white py-2 rounded-full active:scale-90 
-        focus:scale-90 transition-transform font-medium outline-none md:px-10 
-         peer-invalid:bg-red-100
-         peer-required:bg-green-500"
+        focus:scale-90 transition-transform font-medium  md:px-10 
+   "
         >
           Search
         </button>
@@ -23,3 +28,5 @@ export default function Home() {
     </main>
   );
 }
+
+//버튼 연습
